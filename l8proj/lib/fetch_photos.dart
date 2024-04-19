bbe95d9aea6277658e15cb868399670b103b6c07
DonZskoy <donskoidima12345@gmail.com>
2024-04-19 23:49:48 +0300

    Add class Photo and method fetchPhoto

---
+++ l8proj/lib/fetch_photos.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:l8proj/class_photo.dart';
+
+Future<List<Photo>> fetchPhotos(http.Client client) async{
+  final response = await client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+  return compute(parsePhotos, response.body);
+}
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Photo>((json) => Photo.fromJSON(json)).toList();
+}
